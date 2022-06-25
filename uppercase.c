5ffce33527010b9a1d0017833f23c2efb6a56a8d
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-25 09:06:06 -0400

    Create uppercase.c

---
+++ uppercase.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void* upper(char* input)  {
+
+int i; 
+int length;
+length = strlen(input);
+
+
+ for(i=0; i<length; i++){
+putchar(toupper(input[i]));
+}}
+
+
+int main( int argc, char **argv)  { 
+   upper(argv[1]);
+    return 0;
+}
